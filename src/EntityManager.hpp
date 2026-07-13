ef688f91e5fe3715eab1d2326b615c93fc69675e
FabioMijango <00156022@uca.edu.sv>
2026-07-12 18:47:31 -0600

    Docs: add documentations for EntityManager and SparseSet

--- src/EntityManager.hpp
+++ src/EntityManager.hpp
@@ -12,20 +12,38 @@
  *  Handle the creation of entities and the addition/retrieval of components associated with those entities.
  *  Each component type has its own storage, allowing individual access and management of every entity data.
  *
- * @see Scene, SparseSet
+ * @see Scene, SparseSet,
  */
 class EntityManager {
     typedef std::unordered_map<std::type_index, std::unique_ptr<ISparseSet> > ComponentSets;
 
+    /// Next available entity ID
     Entity          m_nextEntity = 1;
+    /// Storage for all component sets, indexed by their type
     ComponentSets   m_componentSets = {};
 
 public:
     EntityManager() = default;
 
+    /**
+     * @brief Creates a new entity and returns its ID.
+     * @return The ID of the created entity.
+     */
     Entity  createEntity();
+
+    /**
+     * @brief Destroys an entity and removes all its associated components.
+     * @param e The ID of the entity to destroy
+     */
     void    destroyEntity(Entity e);
 
+    /**
+     * @brief Adds a component of the specified type to the given entity.
+     * @tparam Component Type of the component to add
+     * @param e The ID of the entity to which to add the component
+     * @param args Arguments taken by the component to be created
+     * @return A pointer to the added component
+     */
     template<ComponentConcept Component, typename... Args>
     Component*  addComponent(Entity e, Args&& ...args) {
         SparseSet<Component> &sparseSet = getSparseSet<Component>();
@@ -33,6 +51,12 @@ public:
         return sparseSet.addComponent(e, Component{ std::forward<Args>(args)...});
     }
 
+    /**
+     * @brief Retrieves a pointer to the component of the specified type associated with the given entity.
+     * @tparam Component Type of the component to retrieve
+     * @param e The ID of the entity for which to retrieve the component
+     * @return A pointer to the requested component, or nullptr if the entity does not have the component
+     */
     template<ComponentConcept Component>
     Component*  getComponent(Entity e) {
         SparseSet<Component> &sparseSet = getSparseSet<Component>();
@@ -40,6 +64,11 @@ public:
         return sparseSet.getComponent(e);
     }
 
+    /**
+     * @brief Removes the component of the specified type from the given entity.
+     * @tparam Component Type of the component to remove
+     * @param e The ID of the entity for which to remove the component
+     */
     template<ComponentConcept Component>
     void    removeComponent(Entity e) {
         SparseSet<Component> &sparseSet = getSparseSet<Component>();
@@ -47,12 +76,23 @@ public:
         sparseSet.removeComponent(e);
     }
 
+    /**
+     * @brief Checks if the given entity has a component of the specified type.
+     * @tparam Component Type of the component to check
+     * @param e The ID of the entity for which to check the component
+     * @return true if the entity has the component, false otherwise
+     */
     template<ComponentConcept Component>
     bool    hasComponent(Entity e) {
         SparseSet<Component> &sparseSet = getSparseSet<Component>();
         return sparseSet.hasComponent(e);
     }
 
+    /**
+     * @brief Retrieves the SparseSet for the specified component type, creating it if it does not exist.
+     * @tparam Component Type of the component for which to retrieve the SparseSet
+     * @return A reference to the SparseSet for the specified component type
+     */
     template<ComponentConcept Component>
     SparseSet<Component>&   getSparseSet() {
         const auto typeIndex = std::type_index(typeid(Component));
--- src/SparseSet.hpp
+++ src/SparseSet.hpp
@@ -5,16 +5,29 @@
 
 #include "Types.hpp"
 
+/**
+ * @interface ISparseSet
+ * @brief Interface for a sparse set of components.
+ */
 class ISparseSet {
 public:
     virtual     ~ISparseSet() = default;
     virtual void removeComponent(Entity e) = 0;
 };
 
+
+/**
+ * @class SparseSet
+ * @brief A sparse set data structure for storing components associated with entities.
+ * @tparam Component The type of component to store in the sparse set.
+ */
 template<ComponentConcept Component>
 class SparseSet : public ISparseSet {
+    /// Dense array of components
     std::vector<Component>   m_denseComponents;
+    /// Dense array of entity keys corresponding to the components
     std::vector<Entity>      m_denseKeys;
+    /// Array for the entities to index into the dense arrays
     std::vector<std::size_t> m_sparse;
 
     static constexpr std::size_t INVALID{std::numeric_limits<std::size_t>::max()};
@@ -22,6 +35,13 @@ class SparseSet : public ISparseSet {
 public:
     SparseSet() = default;
 
+
+    /**
+     * @brief Adds a component for the given entity.
+     * @param e Entity which will add the component.
+     * @param component The component to add.
+     * @return Pointer to the added component.
+     */
     Component* addComponent(Entity e, const Component& component) {
         if (e >= m_sparse.size()) {
             m_sparse.resize(e + 1, INVALID);
@@ -34,6 +54,11 @@ public:
         return &m_denseComponents.back();
     }
 
+    /**
+     * @brief Retrieves the component associated with the given entity.
+     * @param e Entity which component will get
+     * @return Pointer to the component, or nullptr if not found
+     */
     Component*  getComponent(Entity e) {
         if (e >= m_sparse.size()) {
             return nullptr;
@@ -47,6 +72,10 @@ public:
         return &m_denseComponents[index];
     }
 
+    /**
+     * @brief Removes the component associated with the given entity.
+     * @param e Entity which component will be removed.
+     */
     void        removeComponent(Entity e) override {
         {
             if (e >= m_sparse.size()) {
@@ -73,6 +102,12 @@ public:
             m_denseKeys.pop_back();
         }
     }
+
+    /**
+     * @brief Checks if the given entity has a component.
+     * @param e Entity to check.
+     * @return true if the entity has a component, false otherwise.
+     */
     bool        hasComponent(Entity e) const {
         return e < m_sparse.size() && m_sparse[e] != INVALID;
     }
