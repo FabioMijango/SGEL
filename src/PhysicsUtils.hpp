c66df8aeddb06e83a429990c3c21b522e71741c8
FabioMijango <00156022@uca.edu.sv>
2026-07-15 14:50:05 -0600

    Docs: Add documentation to Utils files

--- src/PhysicsUtils.hpp
+++ src/PhysicsUtils.hpp
@@ -3,7 +3,12 @@
 #include "Types.hpp"
 
 namespace sPhysics {
-
+    /**
+     * @brief Get the bounding box of an entity based on its transform and collider components.
+     * @param transform Transform component of the entity.
+     * @param collider Collider component of the entity.
+     * @return Bounding box rectangle.
+     */
     SDL_FRect getBoundingBox(TransformComponent& transform, BoxColliderComponent& collider) {
         SDL_FRect boundingBox = {
             transform.position.x + collider.offset.x,
@@ -14,6 +19,12 @@ namespace sPhysics {
         return boundingBox;
     }
 
+    /**
+     * @brief Check for collision between two axis-aligned bounding boxes (AABBs).
+     * @param rectA First rectangle.
+     * @param rectB Second rectangle.
+     * @return True if the rectangles collide, false otherwise.
+     */
     bool checkAABBCollision(SDL_FRect rectA, SDL_FRect rectB) {
         return rectA.x < rectB.x + rectB.w &&
                rectA.x + rectA.w > rectB.x &&
@@ -21,6 +32,12 @@ namespace sPhysics {
                rectA.y + rectA.h > rectB.y;
     }
 
+    /**
+     * @brief Check for collision between a point and an axis-aligned bounding box (AABB).
+     * @param point Point to check.
+     * @param rect Rectangle to check.
+     * @return True if the point is inside the rectangle, false otherwise.
+     */
     bool checkPointAABBCollision(SDL_Point point, SDL_FRect rect) {
         return point.x >= rect.x && point.x <= rect.x + rect.w &&
                point.y >= rect.y && point.y <= rect.y + rect.h;
