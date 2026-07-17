5392c0d87780e26b85ca8c755ca7dc599a141d42
FabioMijango <00156022@uca.edu.sv>
2026-07-17 13:27:00 -0600

    Add: SGEL header library

---
+++ SGEL.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * @file SGEL.hpp
+ * @brief Simple Game Engine Library (SGEL) header file.
+ */
+
+#include "Action.hpp"
+#include "Animation.hpp"
+#include "Assets.hpp"
+#include "CameraUtils.hpp"
+#include "Engine.hpp"
+#include "EntityManager.hpp"
+#include "PhysicsUtils.hpp"
+#include "Scene.hpp"
+#include "Sprite.hpp"
+#include "Types.hpp"
